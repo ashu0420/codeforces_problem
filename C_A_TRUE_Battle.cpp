f4c4b80dee6b037ae6daf16020778165efb31e5a
ashu <asheesh.iitg@gmail.com>
2024-10-19 22:50:46 +0530

    new file:   A_A_Gift_From_Orangutan.cpp 	new file:   B_Minimise_Oneness.cpp 	new file:   C_A_TRUE_Battle.cpp

---
+++ C_A_TRUE_Battle.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define li long
+#define cn(n) cin >> n
+#define py cout << "YES" << endl
+#define pn cout << "NO" << endl
+#define ct(c) cout << c << endl
+#define fr(dt, a, b) for (dt i = a; i < b; i++)
+#define in(a, n) \
+    fr(ll, 0, n) { cn(a[i]); }
+#define out(a, n)                         \
+    fr(ll, 0, n) { cout << a[i] << " "; } \
+    cout << endl;
+
+using namespace std;
+
+void solve()
+{
+    // Your code here
+    int n;
+    cn(n);
+    string s;
+    cn(s);
+    if(s[0]=='1'||s[n-1]=='1')
+    {
+        py;
+        return;
+    }
+    else 
+    {
+        
+        for(int i=0;i<n-2;i++)
+        {
+            if(s.substr(i,2)=="11")
+            {
+                py;
+                return;
+            }
+        }
+
+    }
+    pn;
+  
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cout.precision(10);
+    cout.setf(ios::fixed);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}

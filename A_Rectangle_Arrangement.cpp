d55e8bb8d79d7eb59438c0ed4bb9f238f3b74fbc
ashu <asheesh.iitg@gmail.com>
2024-10-27 09:13:53 +0530

    new file:   A_Rectangle_Arrangement.cpp

---
+++ A_Rectangle_Arrangement.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define li long
+#define cn(n) cin >> n
+#define py cout << "YES" << endl
+#define pn cout << "NO" << endl
+#define ct(c) cout << c << endl
+#define fr(dt, a, b) for (dt i = a; i < b; i++)
+#define in(a, n) \
+    fr(ll, 0, n) { cn(a[i]); }
+#define out(a, n)                         \
+    fr(ll, 0, n) { cout << a[i] << " "; } \
+    cout << endl;
+
+using namespace std;
+
+void solve()
+{
+    // Your code here
+    int n;
+    cn(n);
+    int h = 0;
+    int w = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+
+        h = max(h, x);
+        w = max(w, y);
+    }
+    int ans = w + h;
+    ans *= 2;
+    ct(ans);
+    return;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cout.precision(10);
+    cout.setf(ios::fixed);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}

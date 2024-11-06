61b47914b28374b0527db7c2005045420124dddb
ashu <asheesh.iitg@gmail.com>
2024-11-07 00:53:01 +0530

    new file:   B_Brightness_Begins.cpp

---
+++ B_Brightness_Begins.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define li long
+#define cn(n) cin >> n
+#define py cout << "YES" << endl
+#define pn cout << "NO" << endl
+#define ct(c) cout << c << endl
+#define fr(dt, a, b) for (dt i = a; i < b; i++)
+#define in(a, n) \
+    fr(ll, 0, n) { cn(a[i]); }
+#define out(a, n) \
+    fr(ll, 0, n) { cout << a[i] << " "; }
+
+using namespace std;
+// ll N = LONG_LONG_MAX;
+// vector<ll> dp(N,-1);
+void solve()
+{
+    // Your code here
+    ll k;
+    cn(k);
+    ll temp = k + int(sqrtl(k) + 0.5);
+        ct(temp);
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    // ios::sync_with_stdio(false);
+    // cin.tie(nullptr);
+    // cout.tie(nullptr);
+    // // cout.precision(10);
+    // cout.setf(ios::fixed);
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}

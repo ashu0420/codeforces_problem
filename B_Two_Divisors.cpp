58f4692052acb1ea5f0f79135ac875dcb0d61235
ashu <asheesh.iitg@gmail.com>
2024-11-09 13:50:10 +0530

    new file:   B_Two_Divisors.cpp

---
+++ B_Two_Divisors.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define li long
+#define cn(n) cin >> n
+#define py cout << "YES" << endl
+#define pn cout << "NO" << endl
+#define ct(c) cout << c << endl
+#define fr(dt, a, b) for (dt i = a; i < b; i++)
+#define in(a, n) \
+    fr(ll, 0, n) { cn(a[i]); }
+#define out(a, n)                         \
+    fr(ll, 0, n) { cout << a[i] << " "; } \
+    cout << endl;
+
+using namespace std;
+
+void solve()
+{
+    // Your code here
+    int a, b;
+    cin >> a >> b;
+    if (b % a == 0)
+    {
+        ct(b * (b / a));
+    }
+    else
+    {
+        ll x = 1LL * b * a;
+        ct(x / gcd(a, b));
+    }
+   
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cout.precision(10);
+    cout.setf(ios::fixed);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}

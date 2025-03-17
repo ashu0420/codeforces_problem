4da45410bc5917a624d09490066f8985e30bdf1b
ashu <asheesh.iitg@gmail.com>
2025-03-17 23:09:36 +0530

    new file:   A_To_Zero.cpp 	new file:   B_Array_Recoloring.cpp

---
+++ B_Array_Recoloring.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define li long
+#define cn(n) cin >> n
+#define py cout << "YES" << endl
+#define pn cout << "NO" << endl
+#define ct(c) cout << c << endl
+#define fr(dt, a, b) for (dt i = a; i < b; i++)
+#define in(a, n) \
+    fr(ll, 0, n) { cn(a[i]); }
+#define out(a, n)                         \
+    fr(ll, 0, n) { cout << a[i] << " "; } \
+    cout << endl;
+#define X ct('X')
+#define sort(a) sort(a.begin(), a.end());
+#define vin(a, n)    \
+    vector<ll> a(n); \
+    in(a, n);
+const int MOD = 1e9 + 7;
+
+using namespace std;
+
+void solve()
+{
+    // Your code here
+    ll n, k;
+    cin >> n >> k;
+    vin(v, n);
+    ll ans = 0;
+    if (k == 1)
+    {
+        for (ll i = 1; i < n - 1; i++)
+        {
+            ans = max({ans, v[i] + v[0], v[i] + v[n - 1]});
+        }
+        ans = max({ans, v[n - 1] + v[0]});
+    }
+    else
+    {
+        sort(v);
+        reverse(v.begin(), v.end());
+        for (ll i = 0; i < n && i < k+1; i++)
+        {
+            ans+=v[i];
+        }
+    }
+    ct(ans);
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cout.precision(10);
+    cout.setf(ios::fixed);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}

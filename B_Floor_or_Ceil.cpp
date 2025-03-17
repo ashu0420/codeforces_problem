3c9e48f641158152d496f781afe820b3011db8a4
ashu <asheesh.iitg@gmail.com>
2025-03-17 20:01:43 +0530

    new file:   B_Floor_or_Ceil.cpp

---
+++ B_Floor_or_Ceil.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define li long
+#define cn(n) cin >> n
+#define py cout << "YES" << endl
+#define pn cout << "NO" << endl
+#define ct(c) cout << c << endl
+#define fr(dt, a, b) for (dt i = a; i < b; i++)
+#define in(a, n) \
+    fr(ll, 0, n) { cn(a[i]); }
+#define out(a, n)                         \
+    fr(ll, 0, n) { cout << a[i] << " "; } \
+    cout << endl;
+#define X ct('X')
+#define sort(a) sort(a.begin(), a.end());
+#define vin(a, n)    \
+    vector<ll> a(n); \
+    in(a, n);
+const int MOD = 1e9 + 7;
+
+using namespace std;
+
+void solve()
+{
+    // Your code here
+    int x, n, m;
+    cin >> x >> n >> m;
+    int y = x;
+    m = min(m, 30);
+    n = min(n, 30);
+    for (int i = 1; i <= n; i++)
+        x /= 2;
+    for (int i = 1; i <= m; i++)
+        x = (x + 1) / 2;
+    for (int i = 1; i <= m; i++)
+        y = (y + 1) / 2;
+    for (int i = 1; i <= n; i++)
+        y /= 2;
+    cout << min(x, y) << " " << max(x, y) << "\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cout.precision(10);
+    cout.setf(ios::fixed);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}

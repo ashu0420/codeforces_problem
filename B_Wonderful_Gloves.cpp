d2fec5fef5b922a721f878a4e6dd378e7786b1a3
ashu <asheesh.iitg@gmail.com>
2025-06-23 08:10:03 +0530

    new file:   A_Wonderful_Sticks.cpp 	new file:   B_Wonderful_Gloves.cpp

---
+++ B_Wonderful_Gloves.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define li long
+#define cn(n) cin >> n
+#define py cout << "YES" << endl
+#define pn cout << "NO" << endl
+#define ct(c) cout << c << endl
+#define fr(dt, a, b) for (dt i = a; i < b; i++)
+#define in(a, n) \
+    fr(ll, 0, n) { cn(a[i]); }
+#define in1(a, n) \
+    fr(ll, 1, n + 1) { cn(a[i]); }
+#define out(a, n)                         \
+    fr(ll, 0, n) { cout << a[i] << " "; } \
+    cout << endl;
+#define out1(a, n)                            \
+    fr(ll, 1, n + 1) { cout << a[i] << " "; } \
+    cout << endl;
+#define X ct('X')
+#define srt(a) sort(a.begin(), a.end());
+#define rev(a) reverse(a.begin(), a.end());
+#define vin(a, n)    \
+    vector<ll> a(n); \
+    in(a, n);
+#define vin1(a, n)       \
+    vector<ll> a(n + 1); \
+    in1(a, n);
+const int MOD = 1e9 + 7;
+
+using namespace std;
+
+void solve()
+{
+    // Your code here
+    int n, k;
+    cin >> n >> k;
+    vin(a, n);
+    vin(b, n);
+    vector<int> ax(n), bx(n);
+    for (int i = 0; i < n; i++)
+    {
+        ax[i] = max(a[i], b[i]);
+        bx[i] = min(a[i], b[i]);
+    }
+    srt(ax);
+    srt(bx);
+    ll ans=0;
+    for(auto i:ax)
+    {
+        ans+=i;
+    }
+    rev(bx);
+    for(int i=0;i<k-1;i++)
+    {
+        ans+=bx[i];
+    }
+    ct(ans+1);
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cout.precision(10);
+    cout.setf(ios::fixed);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}

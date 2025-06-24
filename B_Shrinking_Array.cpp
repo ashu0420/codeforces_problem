b465383f7d833e13b47cb690a588f826333861dd
ashu <asheesh.iitg@gmail.com>
2025-06-24 13:56:01 +0530

    new file:   A_Race.cpp 	new file:   B_Shrinking_Array.cpp

---
+++ B_Shrinking_Array.cpp
@@ -0,0 +1,137 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define li long
+#define cn(n) cin >> n
+#define py cout << "YES" << endl
+#define pn cout << "NO" << endl
+#define ct(c) cout << c << endl
+#define fr(dt, a, b) for (dt i = a; i < b; i++)
+#define in(a, n) \
+    fr(ll, 0, n) { cn(a[i]); }
+#define in1(a, n) \
+    fr(ll, 1, n + 1) { cn(a[i]); }
+#define out(a, n)                         \
+    fr(ll, 0, n) { cout << a[i] << " "; } \
+    cout << endl;
+#define out1(a, n)                            \
+    fr(ll, 1, n + 1) { cout << a[i] << " "; } \
+    cout << endl;
+#define X ct('X')
+#define srt(a) sort(a.begin(), a.end());
+#define rev(a) reverse(a.begin(), a.end());
+#define vin(a, n)    \
+    vector<ll> a(n); \
+    in(a, n);
+#define vin1(a, n)       \
+    vector<ll> a(n + 1); \
+    in1(a, n);
+const int MOD = 1e9 + 7;
+
+using namespace std;
+
+ll mod_pow(ll x, ll y)
+{
+    ll result = 1;
+    x = x % MOD;
+    while (y > 0)
+    {
+        if (y & 1)
+            result = (result * x) % MOD;
+        x = (x * x) % MOD;
+        y >>= 1;
+    }
+    return result;
+}
+
+ll modular_inverse(ll denom)
+{
+    return mod_pow(denom, MOD - 2);
+}
+
+void solve()
+{
+    // Your code here
+    int n;
+    cn(n);
+    vin(a, n);
+    int ans = n;
+    for (int i = 0; i < n - 1; i++)
+    {
+        // ct(ans);
+        if (abs(a[i] - a[i + 1]) <= 1)
+        {
+            ct(0);
+            return;
+        }
+        if (a[i] < a[i + 1])
+        {
+            for (int j = i + 2; j < n; j++)
+            {
+                if (a[j] <= a[i])
+                {
+                    ans = min(ans, j - i - 1);
+                }
+            }
+        }
+        else
+        {
+            for (int j = i + 2; j < n; j++)
+            {
+                if (a[j] >= a[i])
+                {
+                    ans = min(ans, j - i - 1);
+                }
+            }
+        }
+        }
+    for (int i = n - 1; i > 0; i--)
+    {
+        // if (abs(a[i] - a[i + 1]) <= 1)
+        // {
+        //     ct(0);
+        //     return;
+        // }
+        if (a[i] < a[i - 1])
+        {
+            for (int j = i - 2; j >= 0; j--)
+            {
+                if (a[j] <= a[i])
+                {
+                    ans = min(ans, i - j - 1);
+                }
+            }
+        }
+        else
+        {
+            for (int j = i - 2; j >= 0; j--)
+            {
+                if (a[j] >= a[i])
+                {
+                    ans = min(ans, i - j - 1);
+                }
+            }
+        }
+    }
+    if (ans == n)
+    {
+        ct(-1);
+        return;
+    }
+    ct(ans);
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cout.precision(10);
+    cout.setf(ios::fixed);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
